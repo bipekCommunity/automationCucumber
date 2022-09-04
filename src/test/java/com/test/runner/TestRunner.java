531f4c982bb40d713e20bf6f82adf74425a437a5
Burak İPEK <100846097+bipekCommunity@users.noreply.github.com>
2022-09-05 00:09:52 +0300

    log calisan version

--- src/test/java/com/test/runner/TestRunner.java
+++ src/test/java/com/test/runner/TestRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/test/step_def",
         dryRun = false,
-        tags = "@wip04092"
+        tags = "@wip0409"
 )
 public class TestRunner {
 }
--- src/test/java/com/test/step_def/login_stepdef.java
+++ src/test/java/com/test/step_def/login_stepdef.java
@@ -6,8 +6,9 @@ import com.test.pages.LoginPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
-
+@Slf4j
 public class login_stepdef {
 LoginPage loginPage=new LoginPage();
     @Given("kullanici username {string} girer")
@@ -16,13 +17,14 @@ LoginPage loginPage=new LoginPage();
 
 
       loginPage.userName.sendKeys(userName);
+      log.info("Example");
     }
 
     @Given("kullanici password {string} girer")
     public void kullanici_password_girer(String password) {
 
         loginPage.password.sendKeys(password);
-
+        log.info("Example");
     }
     @When("kullanici login butonunu tikladiginda")
     public void kullanici_login_butonunu_tikladiginda() {
@@ -32,6 +34,7 @@ LoginPage loginPage=new LoginPage();
     public void kullanici_login_olabilmelidir() {
         Assert.assertEquals("https://www.saucedemo.com/inventory.htmssssl",Driver.get().getCurrentUrl());
 
+        log.info("currentUrl",Driver.get().getCurrentUrl());
     }
     @Given("Kullanici {string} username {string} password ile login olur")
     public void kullanici_username_password_ile_login_olur(String userName, String password) {
@@ -39,7 +42,7 @@ LoginPage loginPage=new LoginPage();
         loginPage.password.sendKeys(password);
         loginPage.loginButton.click();
         Assert.assertEquals("https://www.saucedemo.com/inventory.html",Driver.get().getCurrentUrl());
-
+        log.info("ExampleAllLogin");
     }
 
 
